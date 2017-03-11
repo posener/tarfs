d23a5111acc91d6e675165e18dd5e0fa41d74165
Eyal Posener <posener@gmail.com>
2017-03-11 13:34:59 +0200

    open: enable openning of not gzipped files

--- open.go
+++ open.go
@@ -7,21 +7,28 @@ import (
 )
 
 // Open is a helper function for opening a tar gz files as file systems
-func Open(name string) (t *tarFS, closeFunc func() error, err error) {
+func Open(name string) (tfs *tarFS, closeFunc func() error, err error) {
 	f, err := os.Open(name)
 	if err != nil {
 		return
 	}
-	z, err := gzip.NewReader(f)
-	if err != nil {
-		return
-	}
-	tgz := tar.NewReader(z)
 
-	closeFunc = func() error {
-		z.Close()
-		return f.Close()
+	var t *tar.Reader
+
+	if z, zipErr := gzip.NewReader(f); zipErr == nil {
+		// that archive is zipped
+		t = tar.NewReader(z)
+		closeFunc = func() error {
+			z.Close()
+			return f.Close()
+		}
+	} else {
+		// assuming that the archive is not gzipped
+		f.Seek(0, os.SEEK_SET)
+		t = tar.NewReader(f)
+		closeFunc = f.Close
 	}
-	t = New(tgz)
+
+	tfs = New(t)
 	return
 }
