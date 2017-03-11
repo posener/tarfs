63c0be3065304b5b740a8bd51020ee1e6b8d65bc
Eyal Posener <posener@gmail.com>
2017-03-11 09:46:31 +0200

    Example: Add documentation

--- examples/example_test.go
+++ examples/example_test.go
@@ -6,13 +6,21 @@ import (
 	"github.com/posener/tarfs"
 )
 
-func ExampleNew() {
+// ExampleTest shows a basic usage of the tarfs package as FileSystem
+// for fs.WalkFS function.
+func ExampleTest() {
+	// tarfs.Open is an helper function to open tar.gz files,
+	// it returns an object that implements the FileSystem interface
+	// and a function to close the opened file.
 	f, close, err := tarfs.Open("./root.tar.gz")
 	if err != nil {
 		panic(err)
 	}
 	defer close()
 
+	// WalkFS accepts an object that implements the FileSystem interface,
+	// give it the tarFS object created above to walk over the files in the
+	// tar file.
 	walker := fs.WalkFS("/", f)
 	for walker.Step() {
 		fmt.Println(walker.Path()) // prints all the paths in the tar file
